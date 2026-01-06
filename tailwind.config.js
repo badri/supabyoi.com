10f21a66ee71fe53c7e3d7fa3e8f96a2244d7c16
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2026-01-06 11:51:42 +0530

    Initial landing page: Tailwind, minimal dev-focused copy

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'supa-green': '#3ECF8E',
+        'supa-dark': '#1C1C1C',
+      },
+    },
+  },
+  plugins: [],
+}
